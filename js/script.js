c7ef1170e7e6a46004e2b2b86fc5e179b56825af
Denis Toloshny <gg.zyus@gmail.com>
2016-09-03 23:55:41 +0300

    анимация ухода формы?

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
   var link = document.getElementsByClassName('form-content');
   var link2 = document.getElementsByClassName('form-heading')
 
+  var isShown = false;
   link[0].style.visibility = "hidden";
   
   link2[0].addEventListener("click", function(event){
     event.preventDefault();
-    link[0].classList.add('form-content-animate');
-    link[0].style.visibility = "visible";
+    link[0].classList.remove('form-content-animate-up');
+    link[0].classList.remove('form-content-animate-down');
+  
+    if (!isShown){
+      link[0].classList.add('form-content-animate-up');
+      link[0].style.visibility = "visible";
+      isShown = true;
+    }
+    else if (isShown) {
+      link[0].classList.add('form-content-animate-down');
+      link[0].style.visibility = "hidden"; 
+      isShown = false;
+    }
   });
